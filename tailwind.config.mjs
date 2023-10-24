96ad800c4cd3bf673f6750fc670111ea32fa5a7c
UnCompa1777 <brandonowo26@gmail.com>
2023-10-24 07:29:44 -0500

    Cambios menores x2

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"Gabarito": ["'Gabarito', sans-serif;"],
+				"Montserrat": ["'Montserrat', sans-serif"],
+				"Poppins": ["'Poppins', sans-serif;"],
+				"Roboto": ["'Roboto', sans-serif;"]
+			}
+		},
 	},
 	plugins: [],
 }
